5c6002350fa373ba4cd8489df93ffac83cc0ca65
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-11-30 14:46:20 +0100

    variable file added

---
+++ Variable.py
@@ -0,0 +1,13 @@
+# Rules for python variable
+'''
+ # A variable can have a short name (like x and y) or a more descriptive name
+ # A vaiable name must start with a letter or the underscore character
+ # A variable name cannot start with a number
+ # A variable name can only contain alpha-numeric characters and underscore(_,A-Z,a-z,0-9)
+ # Variable names are case-sensitive(age,Age and AGE are different meaning)
+'''
+
+x,y,z=1,2,3
+print(x)
+str_05=52
+print(str_05)
